6c8174cfe882cd6d32652ad7c6c5a09a23076875
radishengine <radishengine@gmail.com>
2018-12-12 11:58:17 +0000

    Update barf-main.js

--- barf-main.js
+++ barf-main.js
@@ -11,6 +11,10 @@ function(
 
   'use strict';
   
+  function loadBytes(bytes) {
+    console.log(bytes);
+  }
+  
   function getTemplate(query) {
     var node = document.querySelector('#templates > ' + query);
     if (!node) return null;
@@ -19,7 +23,33 @@ function(
   }
   
   const content = document.getElementById('content');
-  const dropzone = getTemplate('.dropzone');
+  const dropzone = Object.assign(getTemplate('.dropzone'), {
+    dragCount: 0,
+    ondragenter: function(e) {
+      if (++this.dragCount === 1) {
+        this.classList.add('dropping');
+      }
+      e.preventDefault();
+    },
+    ondragleave: function(e) {
+      if (--this.dragCount === 0) {
+        this.classList.remove('dropping');
+      }
+      e.preventDefault();
+    },
+    ondrop: function(e) {
+      this.dragCount = 0;
+      e.preventDefault();
+      const files = e.dataTransfer && e.dataTransfer.files;
+      if (files && files[0]) {
+        Object.assign(new FileReader, {
+          onload: function(e) {
+            loadBytes(new Uint8Array(this.result));
+          },
+        }).readAsArrayBuffer(files[0]);
+      }
+    },
+  });
   
   content.appendChild(dropzone);
 
